dc91b9fd9bd880ae28b0d64b1052d31726b29ea1
Kai Siebenrock <kai.siebenrock@whu.edu>
2019-05-18 16:21:50 +0200

    Add grid and tabs.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/65064b42-5a8f-4981-b4ae-3c6e38ef8b04/deploy-status)](https://app.netlify.com/sites/dasks/deploys)
 
+## Notes
+
+- Vue 3.7.0
+
 ## Project setup
 
 ```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
-  <v-container fluid fill-height class="home-hero">
-    <v-layout justify-center align-center column pa-5>
-      <h1>Open Tasks</h1>
-    </v-layout>
-  </v-container>
+  <span>
+    <v-container fluid fill-height class="home-hero">
+      <v-layout justify-center align-center column pa-5>
+        <h1>Open Tasks</h1>
+      </v-layout>
+    </v-container>
+
+    <v-tabs fixed-tabs color="rgba(255, 0, 0, 0)">
+      <v-tab to="/open">Open Tasks</v-tab>
+      <v-tab to="/completed">Completed Tasks</v-tab>
+      <v-tab to="/validated">Validated Tasks</v-tab>
+    </v-tabs>
+
+    <v-container grid-list-md text-xs-center>
+      <v-layout row wrap>
+        <v-flex v-for="i in 4" :key="`3${i}`" xs3>
+          <v-card dark color="secondary">
+            <v-card-text class="px-0">3</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </span>
 </template>
 
 <script>
