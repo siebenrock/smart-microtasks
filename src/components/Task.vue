80e17084a27b7b51720eef8223fc9a03afd58390
Kai Siebenrock <kai.siebenrock@whu.edu>
2019-05-18 19:33:27 +0200

    Add sample card.

---
+++ src/components/Task.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card color="light-blue lighten-4">
+    <v-card-title primary-title>
+      <div>
+        <h3 class="headline">Super Micro Task</h3>
+        <div>Deadline: 20.05.2019</div>
+        <div>Reward: $5.00</div>
+      </div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Task",
+};
+</script>
+
+<style scoped>
+.headline {
+  margin-bottom: 12px;
+}
+</style>
