d1255807d8593d2f817dc679dd3b346d3718ab79
Kai Siebenrock <kai.siebenrock@whu.edu>
2019-05-18 14:55:20 +0200

    Install vue and add vuetify.

---
+++ microtasks/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <navigation></navigation>
+  </v-app>
+</template>
+
+<script>
+import Navigation from "@/components/Navigation";
+
+export default {
+  name: "App",
+  components: {
+    Navigation,
+  },
+};
+</script>
+
+<style></style>
