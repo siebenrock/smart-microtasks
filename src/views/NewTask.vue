0d4ed673c75f3d6501b96c1086c9b496a5fcf81d
Kai Siebenrock <kai.siebenrock@whu.edu>
2019-05-18 17:26:48 +0200

    Implement input fields when adding new tasks.

--- src/views/NewTask.vue
+++ src/views/NewTask.vue
@@ -2,49 +2,72 @@
   <span>
     <v-stepper v-model="e1">
       <v-stepper-header>
-        <v-stepper-step :complete="e1 > 1" step="1">Description</v-stepper-step>
-        <v-divider></v-divider>
-        <v-stepper-step :complete="e1 > 2" step="2">Details</v-stepper-step>
-        <v-divider></v-divider>
-        <v-stepper-step step="3">Reward</v-stepper-step>
+        <v-container fill-height>
+          <v-stepper-step :complete="e1 > 1" step="1"
+            >Description</v-stepper-step
+          >
+          <v-divider></v-divider>
+          <v-stepper-step :complete="e1 > 2" step="2">Details</v-stepper-step>
+          <v-divider></v-divider>
+          <v-stepper-step step="3">Reward</v-stepper-step>
+        </v-container>
       </v-stepper-header>
 
       <v-stepper-items>
-        <v-stepper-content step="1">
-          <v-card class="mb-5" color="grey lighten-1" height="200px"
-            >Step 3</v-card
-          >
+        <v-container fill-height>
+          <v-stepper-content step="1">
+            <v-textarea
+              outline
+              name="Description"
+              label="Description"
+              value="Please enter a detailed description of the task."
+              clearable="true"
+              autofocus="true"
+            ></v-textarea>
 
-          <v-btn color="primary" @click="e1 = 2">
-            Continue
-          </v-btn>
+            <v-btn color="primary" @click="e1 = 2">
+              Continue
+            </v-btn>
+          </v-stepper-content>
 
-          <v-btn flat>Cancel</v-btn>
-        </v-stepper-content>
+          <v-stepper-content step="2">
+            <v-layout justify-center align-center>
+              <v-date-picker
+                v-model="picker"
+                first-day-of-week="1"
+              ></v-date-picker>
+            </v-layout>
 
-        <v-stepper-content step="2">
-          <v-card class="mb-5" color="grey lighten-1" height="200px"
-            >Step 2</v-card
-          >
+            <v-btn color="primary" @click="e1 = 3">
+              Continue
+            </v-btn>
+          </v-stepper-content>
 
-          <v-btn color="primary" @click="e1 = 3">
-            Continue
-          </v-btn>
+          <v-stepper-content step="3">
+            <v-card class="mb-5" color="grey lighten-1" height="200px"
+              >Step 3</v-card
+            >
 
-          <v-btn flat>Cancel</v-btn>
-        </v-stepper-content>
+            <v-text-field
+              label="MyAddress"
+              placeholder="00000"
+              disabled="true"
+              outline
+            ></v-text-field>
 
-        <v-stepper-content step="3">
-          <v-card class="mb-5" color="grey lighten-1" height="200px"
-            >Step 3</v-card
-          >
+            <v-text-field
+              label="MyAddress"
+              placeholder="00000"
+              clearable="true"
+              autofocus="true"
+              outline
+            ></v-text-field>
 
-          <v-btn color="primary" @click="e1 = 1">
-            Continue
-          </v-btn>
-
-          <v-btn flat>Cancel</v-btn>
-        </v-stepper-content>
+            <v-btn color="primary" @click="e1 = 1">
+              Continue
+            </v-btn>
+          </v-stepper-content>
+        </v-container>
       </v-stepper-items>
     </v-stepper>
   </span>
@@ -55,9 +78,17 @@ export default {
   data() {
     return {
       e1: 0,
+      picker: new Date().toISOString().substr(0, 10),
     };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+li a {
+  text-decoration: none;
+}
+div {
+  text-decoration: none;
+}
+</style>
