42da9b415e5faafd1f6c422757671160dc382054
Kai Siebenrock <kai.siebenrock@whu.edu>
2019-05-19 11:45:31 +0200

    Optimise mobile.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <v-layout justify-center align-center column pa-5>
         <h1>Open Tasks</h1>
 
-        <v-layout row>
+        <v-layout row wrap>
           <v-card dark color="light-blue darken-2" class="me">
             <v-list-tile>
               <v-list-tile-action>
@@ -34,15 +34,15 @@
     </v-container>
 
     <v-tabs fixed-tabs color="rgba(255, 0, 0, 0)">
-      <v-tab @click="tab = 0">Open Tasks</v-tab>
-      <v-tab @click="tab = 1">Completed Tasks</v-tab>
-      <v-tab @click="tab = 2">Validated Tasks</v-tab>
+      <v-tab @click="tab = 0">Open</v-tab>
+      <v-tab @click="tab = 1">Completed</v-tab>
+      <v-tab @click="tab = 2">Validated</v-tab>
       <!-- to="/validated"  -->
     </v-tabs>
 
     <v-container fluid grid-list-md>
       <v-layout row wrap>
-        <v-flex v-for="i in 16" :key="`3${i}`" xs3> <Task></Task></v-flex>
+        <v-flex v-for="i in 16" :key="`3${i}`" xs6 md3> <Task></Task></v-flex>
       </v-layout>
     </v-container>
   </span>
@@ -65,12 +65,16 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+  margin-bottom: 15px;
+}
 .home-hero {
   width: 100%;
-  height: 20%;
+  height: 250px;
+  padding-bottom: 20px;
 }
 .me {
-  margin: 20px 5px 10px;
+  margin: 5px;
   width: 250px;
   height: 50px;
 }
