ec6574479b8cf6b8ab33734436f8011b2f0e57af
Raúl Berganza <brgraul@gmail.com>
2019-05-19 10:22:25 +0200

    Addition of IPFS upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,8 +14,21 @@
 
     <v-container grid-list-md text-xs-center>
       <v-layout row wrap>
-        <v-flex v-for="i in 4" :key="`3${i}`" xs3>
+        <v-flex xs3>
           <v-card dark color="secondary">
+        <form action="/">
+          <fieldset>
+            <legend>Upload photo</legend>
+            <input type="file" name="photo" id="photo">
+            <a @click="upload">Upload</a>
+          </fieldset>
+        </form>
+        </br>
+        </br>
+        <a id="url"></a>
+        </br>
+        </br>
+        <img id="output">
             <v-card-text class="px-0">3</v-card-text>
           </v-card>
         </v-flex>
@@ -26,6 +39,31 @@
 
 <script>
 export default {
+  methods:{
+    upload: function() {
+      const reader = new FileReader();
+      reader.onloadend = function() {
+        const ipfs = window.IpfsApi('localhost', 5001) // Connect to IPFS
+        const buf = buffer.Buffer(reader.result) // Convert data into buffer
+        ipfs.files.add(buf, (err, result) => { // Upload buffer to IPFS
+
+
+          if(err) {
+            console.error(err)
+            return
+          }
+          let url = `https://ipfs.io/ipfs/${result[0].hash}`
+          console.log(`Url --> ${url}`)
+          document.getElementById("url").innerHTML= url
+          document.getElementById("url").href= url
+          document.getElementById("output").src = url
+        })
+      }
+      const photo = document.getElementById("photo");
+      console.log(photo.files[0])
+      reader.readAsArrayBuffer(photo.files[0]); // Read Provided File
+    }
+  },
   name: "home",
 };
 </script>
