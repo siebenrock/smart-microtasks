2ffe2930831027dcf80687ce76265c55c819e408
Michael Froehlich <m.froehlich1994@gmail.com>
2019-05-19 12:56:38 +0200

    Add Task to trigger deploy

---
+++ src/models/Task.js
@@ -0,0 +1,17 @@
+
+class Task {
+
+    // creates a JS Task from ethereum
+	constructor (ethereum_task) {
+        console.log(ethereum_task)
+        
+        // owner
+        // deadline
+        // reward
+        // title
+        // description
+    }
+    
+}
+
+export default MTMTContractWorker;
