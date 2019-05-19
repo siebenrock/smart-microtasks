c386506a3ff0a1001ca2163fb245bd58a3951342
Kai Siebenrock <kai.siebenrock@whu.edu>
2019-05-20 00:36:24 +0200

    deploy

---
+++ precache-manifest.f2efb2ec8cc279fea77bf6c5e88634a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/smart-microtasks/robots.txt"
+  },
+  {
+    "revision": "756d226bc0806d1071bf",
+    "url": "/smart-microtasks/js/chunk-vendors.f306885e.js"
+  },
+  {
+    "revision": "b85551874241b12a11b9",
+    "url": "/smart-microtasks/js/app.058ecaae.js"
+  },
+  {
+    "revision": "b72d50743f11267690cf",
+    "url": "/smart-microtasks/js/about.8fce3fc9.js"
+  },
+  {
+    "revision": "7cf8686358c14da9b7ffee19d304e890",
+    "url": "/smart-microtasks/index.html"
+  },
+  {
+    "revision": "756d226bc0806d1071bf",
+    "url": "/smart-microtasks/css/chunk-vendors.23a3498f.css"
+  },
+  {
+    "revision": "b85551874241b12a11b9",
+    "url": "/smart-microtasks/css/app.0c9ecedf.css"
+  },
+  {
+    "revision": "b72d50743f11267690cf",
+    "url": "/smart-microtasks/css/about.c31a8717.css"
+  },
+  {
+    "revision": "e11bf1ee682968f00680983c8c913c32",
+    "url": "/smart-microtasks/bundle.js"
+  }
+];
